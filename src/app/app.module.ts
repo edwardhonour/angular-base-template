baf4a49c64edc9573c33eb68f93c13caa9fdee1c
Ed Honour <40154656+edwardhonour@users.noreply.github.com>
2022-11-29 14:52:25 -0600

    Angular Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { NgxTablePaginationModule } from 'ngx-table-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgxEditorModule } from 'ngx-editor'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxTablePaginationModule,
+    Ng2SearchPipeModule,
+    NgxEditorModule
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
